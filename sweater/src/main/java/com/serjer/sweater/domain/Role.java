4d127775b9840863da5d3eff7b8ec64fd1e7b44a
Serzas <j_sergej@yahoo.com>
2019-11-06 12:24:17 +0200

    +jpa, security

---
+++ sweater/src/main/java/com/serjer/sweater/domain/Role.java
@@ -0,0 +1,5 @@
+package com.serjer.sweater.domain;
+
+public enum Role {
+	USER;
+}
---
+++ sweater/src/test/java/com/serjer/SweaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serjer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
